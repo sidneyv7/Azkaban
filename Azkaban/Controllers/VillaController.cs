bca71b471d413155225764176ddc040027cf84df
sidneyv7 <hadi.kootzadeh@gmail.com>
2023-11-14 12:03:12 +0330

    CH7

--- Azkaban/Controllers/VillaController.cs
+++ Azkaban/Controllers/VillaController.cs
@@ -7,10 +7,11 @@ namespace Azkaban.Controllers
   public class VillaController : Controller
   {
     private readonly ApplicationDbContext _db;
-
-    public VillaController(ApplicationDbContext db)
+    private readonly IWebHostEnvironment _webHostEnvironment;
+    public VillaController(ApplicationDbContext db,IWebHostEnvironment webHostEnvironment)
     {
       _db = db;
+_webHostEnvironment = webHostEnvironment;
     }
 
     public IActionResult Index()
@@ -33,6 +34,22 @@ namespace Azkaban.Controllers
 
       if (ModelState.IsValid)
       {
+        if (obj.Image != null)
+        {
+          string fileName = Guid.NewGuid().ToString() + Path.GetExtension(obj.Image.FileName);
+          string imagePath = Path.Combine(_webHostEnvironment.WebRootPath, @"images\VillaImage");
+
+          using var fileStream = new FileStream(Path.Combine(imagePath, fileName), FileMode.Create);
+          obj.Image.CopyTo(fileStream);
+
+          obj.ImageUrl = @"\images\VillaImage\" + fileName;
+        }
+        else
+        {
+          obj.ImageUrl = "https://placehold.co/600x400";
+        }
+
+
         _db.Villas.Add(obj);
         _db.SaveChanges();
         TempData["success"] = "The villa has been created successfully.";
@@ -59,6 +76,27 @@ namespace Azkaban.Controllers
     {
       if (ModelState.IsValid && obj.Id > 0)
       {
+        if (obj.Image != null)
+        {
+          string fileName = Guid.NewGuid().ToString() + Path.GetExtension(obj.Image.FileName);
+          string imagePath = Path.Combine(_webHostEnvironment.WebRootPath, @"images\VillaImage");
+
+          if (!string.IsNullOrEmpty(obj.ImageUrl))
+          {
+            var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, obj.ImageUrl.TrimStart('\\'));
+
+            if (System.IO.File.Exists(oldImagePath))
+            {
+              System.IO.File.Delete(oldImagePath);
+            }
+          }
+
+          using var fileStream = new FileStream(Path.Combine(imagePath, fileName), FileMode.Create);
+          obj.Image.CopyTo(fileStream);
+
+          obj.ImageUrl = @"\images\VillaImage\" + fileName;
+        }
+
         _db.Villas.Update(obj);
         _db.SaveChanges();
         TempData["success"] = "The villa has been updated successfully.";
@@ -84,6 +122,15 @@ namespace Azkaban.Controllers
       Villa? objFromDb = _db.Villas.FirstOrDefault(u => u.Id == obj.Id);
       if (objFromDb is not null)
       {
+        if (!string.IsNullOrEmpty(objFromDb.ImageUrl))
+        {
+          var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, objFromDb.ImageUrl.TrimStart('\\'));
+
+          if (System.IO.File.Exists(oldImagePath))
+          {
+            System.IO.File.Delete(oldImagePath);
+          }
+        }
         _db.Villas.Remove(objFromDb);
         _db.SaveChanges();
         TempData["success"] = "The villa has been deleted successfully.";
